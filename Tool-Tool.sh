f2887bfd7d224abea41afc2fc56ff68208d01f10
kakathic <kakathic@gmail.com>
2020-08-28 10:02:42 +0700

    Add files via upload

--- Tool-Tool.sh
+++ Tool-Tool.sh
@@ -1,8 +1,8 @@
 # kakathic - Tool Mod - Hiếu Tools
-curl -# https://raw.githubusercontent.com/kakathic/tool-tool/master/File/Tool-Tool -o /sdcard/Tool-Tool
-curl -# https://raw.githubusercontent.com/kakathic/tool-tool/master/File/Tool-Chmod -o /sdcard/Tool-Chmod
-su -c "cp -rf /sdcard/Tool-Tool /bin"
-su -c "cp -rf /sdcard/Tool-Chmod /bin"
-rm -rf /sdcard/Tool-Chmod
-rm -rf /sdcard/Tool-Tool
+curl -# https://raw.githubusercontent.com/kakathic/tool-tool/master/File/Tool-Tool -o /sdcard/MIUI/Tool-Tool
+curl -# https://raw.githubusercontent.com/kakathic/tool-tool/master/File/Tool-Chmod -o /sdcard/MIUI/Tool-Chmod
+su -c "cp -rf /sdcard/MIUI/Tool-Tool /bin"
+su -c "cp -rf /sdcard/MIUI/Tool-Chmod /bin"
+rm -rf /sdcard/MIUI/Tool-Chmod
+rm -rf /sdcard/MIUI/Tool-Tool
 su -c "Tool-Tool"
