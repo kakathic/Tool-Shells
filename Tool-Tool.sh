dcc49851779904ee81523521d2572a7c007940bf
kakathic <kakathic@gmail.com>
2020-09-01 14:34:20 +0700

    Update Tool-Tool.sh

--- Tool-Tool.sh
+++ Tool-Tool.sh
@@ -1,12 +1,9 @@
 # kakathic - Tool Mod - Hiếu Tools
-curl -# https://raw.githubusercontent.com/kakathic/tool-tool/master/File/Tool-Tool.sh -o /sdcard/MIUI/Tool-Tool2
+curl -# https://raw.githubusercontent.com/kakathic/tool-tool/master/File/Tool-Tool.sh -o /sdcard/MIUI/Tool-Tool
 mount -o rw,remount / 2> /dev/null
 mount -o rw,remount /system 2> /dev/null
-cp -rf /sdcard/MIUI/Tool-Tool2 /bin
-rm -rf /sdcard/MIUI/Tool-Tool2
-chmod -R 777 /sdcard/MIUI/Tool-Tool2
+mv -f /sdcard/MIUI/Tool-Tool /bin/Tool-Tool
+chmod -R 777 /sdcard/MIUI/Tool-Tool
 mount -o ro,remount /system 2> /dev/null
 mount -o ro,remount / 2> /dev/null
-vip=`pwd`
-rm -rf /$vip/Tool-To*.sh
-su -c "Tool-Tool2"
+su -c "Tool-Tool"
